a4cdcc1c0cf2585a9102fa98aaf9b4320cc46100
xarantolus <xarantolus@protonmail.com>
2025-02-16 09:44:01 +0100

    Stop before remove

--- config.sh
+++ config.sh
@@ -3,6 +3,12 @@ set -eoux pipefail
 
 CONTAINER_NAME="osiris-hardware-ci"
 
+# If a running container with the same name exists, stop it.
+if docker ps -q -f name="^${CONTAINER_NAME}$" | grep -q .; then
+  echo "Stopping running container ${CONTAINER_NAME}..."
+  docker stop "${CONTAINER_NAME}" || true
+fi
+
 # If there's any container with the same name, remove it.
 if docker ps -aq -f name="^${CONTAINER_NAME}$" | grep -q .; then
   echo "Removing existing container ${CONTAINER_NAME}..."
